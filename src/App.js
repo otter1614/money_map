2c02aebbc1ce79f94a695365c0e2843489dd3f2e
rb1o1o <rio852000@naver.com>
2025-11-16 20:33:36 +0900

    csv 내보내기/가져오기 + 전체 데이터 초기화 로직 및 구현

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@
 import React from "react";
 import "./styles.css";
 import BudgetPage from "./components/BudgetPage";
+import DataManagementUI from "./components/DataManagementUI";
+
 
 function App() {
-  return <BudgetPage />;
+  return (
+    <div className="App">
+      {/* <BudgetPage /> */}
+      <DataManagementUI />
+    </div>  
+  )
 }
 export default App;
